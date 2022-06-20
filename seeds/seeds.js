a80e653fbc54c2baaae562908ed77e47bbee7b31
ppratham0811 <ppratham0811@gmail.com>
2022-06-20 14:15:10 +0530

    seeds db

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require("./cities");
-const {places,descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
-.then(() => {
-    console.log("connected to db");
-})
-.catch(err => {
-    console.log(err);
-});
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i<50;i++) {
-        const price = Math.floor(Math.random()*5000 + 1000);
-        const random1000 = Math.floor(Math.random()*1000);
+    for (let i = 0; i < 50; i++) {
+        const price = Math.floor(Math.random() * 5000 + 1000);
+        const random1000 = Math.floor(Math.random() * 1000);
         const lenPlaces = places.length;
         const lenDescriptors = descriptors.length;
-        const pickRandomPlace = Math.floor(Math.random()*lenPlaces);
-        const pickRandomDesc = Math.floor(Math.random()*lenDescriptors);
+        const pickRandomPlace = Math.floor(Math.random() * lenPlaces);
+        const pickRandomDesc = Math.floor(Math.random() * lenDescriptors);
         const c = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             price,
@@ -35,5 +35,3 @@ seedDB().then(() => {
     mongoose.connection.close();
     // will exit after the file runs, the connection establishes, and the data saved.
 });
-
-
