bf5bf26c916cb27364c329c5696b155e7bc78fd0
ppratham0811 <ppratham0811@gmail.com>
2022-06-20 14:16:40 +0530

    Joi schemas for verification

--- JoiSchemas.js
+++ JoiSchemas.js
@@ -1,11 +1,18 @@
 const Joi = require('joi');
 
-const campgroundSchema = Joi.object({
-    title: Joi.string().required(),
-    location: Joi.string().required(),
-    price: Joi.number().min(0).required(),
-    description: Joi.string().required(),
-    image: Joi.string().required()
-}).required();
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        location: Joi.string().required(),
+        price: Joi.number().min(0).required(),
+        description: Joi.string().required(),
+        image: Joi.string().required()
+    }).required()
+})
 
-module.exports.campgroundSchema = campgroundSchema;
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().min(1).max(5).required(),
+        body: Joi.string().required()
+    }).required()
+})
