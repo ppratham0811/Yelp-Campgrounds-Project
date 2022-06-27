e1b3422be2cf8dea52e4fc6d0a4e9cb630771f89
ppratham0811 <ppratham0811@gmail.com>
2022-06-27 17:47:25 +0530

    added author feature

--- models/campground.js
+++ models/campground.js
@@ -1,31 +1,35 @@
-const mongoose = require('mongoose');
-const Review = require('./review');
-const {Schema, model} = mongoose;
+const mongoose = require("mongoose");
+const Review = require("./review");
+const { Schema, model } = mongoose;
 
 const campgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
-    description: String, 
+    description: String,
     location: String,
     reviews: [
         {
             type: Schema.Types.ObjectId,
-            ref: "Review"
-        }
-    ]
+            ref: "Review",
+        },
+    ],
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
 });
 
-campgroundSchema.post('findOneAndDelete',async function(camp) {
+campgroundSchema.post("findOneAndDelete", async function (camp) {
     if (camp.reviews.length) {
         await Review.deleteMany({
             _id: {
-                $in: camp.reviews
-            }
+                $in: camp.reviews,
+            },
         });
     }
-})
+});
 
-const Campground = model("Campground",campgroundSchema);
+const Campground = model("Campground", campgroundSchema);
 
 module.exports = Campground;
