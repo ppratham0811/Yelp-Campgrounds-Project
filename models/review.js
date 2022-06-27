5ad2e73dd54fa133ff23a3147478c7c0ca34b27c
ppratham0811 <ppratham0811@gmail.com>
2022-06-27 17:47:49 +0530

    added review author feature

--- models/review.js
+++ models/review.js
@@ -3,7 +3,11 @@ const {Schema, model} = mongoose;
 
 const reviewSchema = new Schema({
     body: String,
-    rating: Number
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
 })
 
 const Review = model("Review",reviewSchema);
