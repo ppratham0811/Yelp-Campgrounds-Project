0d4568cd38d346827941a5f21492e320a4ba4e2e
ppratham0811 <ppratham0811@gmail.com>
2022-06-30 18:07:07 +0530

    aws s3 upload and delete functions

---
+++ aws/index.js
@@ -0,0 +1,31 @@
+const aws = require("aws-sdk");
+const { S3 } = require("aws-sdk");
+const uuid = require("uuid").v4;
+const s3 = new S3();
+
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+module.exports.s3Upload = async (files) => {
+    const params = files.map((file) => {
+        return {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: `uploads/${uuid()}-${file.originalname}`,
+            Body: file.buffer,
+        };
+    });
+
+    return await Promise.all(params.map((param) => s3.upload(param).promise()));
+};
+
+module.exports.s3Delete = async (filename) => {
+    const deleteParams = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: `uploads/${filename}`,
+    };
+    await s3.deleteObject(deleteParams).promise();
+    return;
+};
