042295a07994e3a71028dfeaecc788ca2fea159b
ppratham0811 <ppratham0811@gmail.com>
2022-06-30 18:10:59 +0530

    validation for image

--- JoiSchemas.js
+++ JoiSchemas.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -6,13 +6,16 @@ module.exports.campgroundSchema = Joi.object({
         location: Joi.string().required(),
         price: Joi.number().min(0).required(),
         description: Joi.string().required(),
-        image: Joi.string().required()
-    }).required()
-})
+        images: Joi.object({
+            url: Joi.string().required(),
+            filename: Joi.string().required(),
+        }),
+    }).required(),
+});
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().min(1).max(5).required(),
-        body: Joi.string().required()
-    }).required()
-})
+        body: Joi.string().required(),
+    }).required(),
+});
