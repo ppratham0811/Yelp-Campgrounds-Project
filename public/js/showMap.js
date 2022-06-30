28deee2468cc31e1bf9c9ac61cf94de36c3f6ff5
ppratham0811 <ppratham0811@gmail.com>
2022-06-30 18:07:55 +0530

    map features

---
+++ public/js/showMap.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = mapToken;
+var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: campground.geometry.coordinates,
+    zoom: 10,
+});
+
+let mapMarker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `${campground.title}<br>${campground.location}`
+        )
+    )
+    .addTo(map);
