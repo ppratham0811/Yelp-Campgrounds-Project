2c4b3a782e8729d0862e4a09cc005e922ba1cf8c
ppratham0811 <ppratham0811@gmail.com>
2022-07-12 19:14:51 +0530

    properties.popupText virtual added

--- models/campground.js
+++ models/campground.js
@@ -2,38 +2,47 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const { Schema, model } = mongoose;
 
-const campgroundSchema = new Schema({
-    title: String,
-    images: [
-        {
-            url: String,
-            filename: String,
+const opts = { toJSON: { virtuals: true } };
+
+const campgroundSchema = new Schema(
+    {
+        title: String,
+        images: [
+            {
+                url: String,
+                filename: String,
+            },
+        ],
+        price: Number,
+        description: String,
+        location: String,
+        geometry: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+            },
         },
-    ],
-    price: Number,
-    description: String,
-    location: String,
-    geometry: {
-        type: {
-            type: String,
-            enum: ["Point"],
-            required: true,
-        },
-        coordinates: {
-            type: [Number],
-            required: true,
-        },
-    },
-    reviews: [
-        {
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review",
+            },
+        ],
+        author: {
             type: Schema.Types.ObjectId,
-            ref: "Review",
+            ref: "User",
         },
-    ],
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
     },
+    opts
+);
+
+campgroundSchema.virtual("properties.popupText").get(function() {
+    return `<a href="/campgrounds/${this._id}" target="_blank">${this.title}</a><br><small>${this.location}</small>`;
 });
 
 campgroundSchema.post("findOneAndDelete", async function (camp) {
