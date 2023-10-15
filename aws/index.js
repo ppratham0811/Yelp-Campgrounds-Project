d283c0177a71c69835846bf122435f5cd79f8bc7
ppratham0811 <ppratham0811@gmail.com>
2023-10-15 21:12:03 +0530

    fixed env clash

--- aws/index.js
+++ aws/index.js
@@ -4,28 +4,28 @@ const uuid = require("uuid").v4;
 const s3 = new S3();
 
 aws.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_PRATHAM,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_PRATHAM,
+  region: process.env.AWS_REGION_PRATHAM,
 });
 
 module.exports.s3Upload = async (files) => {
-    const params = files.map((file) => {
-        return {
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
-            Key: `uploads/${uuid()}-${file.originalname}`,
-            Body: file.buffer,
-        };
-    });
+  const params = files.map((file) => {
+    return {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: `uploads/${uuid()}-${file.originalname}`,
+      Body: file.buffer,
+    };
+  });
 
-    return await Promise.all(params.map((param) => s3.upload(param).promise()));
+  return await Promise.all(params.map((param) => s3.upload(param).promise()));
 };
 
 module.exports.s3Delete = async (filename) => {
-    const deleteParams = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: `uploads/${filename}`,
-    };
-    await s3.deleteObject(deleteParams).promise();
-    return;
+  const deleteParams = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: `uploads/${filename}`,
+  };
+  await s3.deleteObject(deleteParams).promise();
+  return;
 };
