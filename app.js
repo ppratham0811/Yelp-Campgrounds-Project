1a1ac318f4b706259fb64c48352ab366fec77238
ppratham0811 <ppratham0811@gmail.com>
2023-10-16 20:10:23 +0530

    fixed cors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 const express = require("express");
 const app = express();
@@ -13,7 +13,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const multer = require("multer");
 const secret = process.env.SECRET || "yelpcampsessionsecret";
-const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL;
 const localDb = "mongodb://localhost:27017/yelp-camp";
 const MongoStore = require("connect-mongo");
 
@@ -21,14 +21,14 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 
 mongoose
-    // .connect(localDb)
-    .connect(dbUrl)
-    .then(() => {
-        console.log("database connected, connection open");
-    })
-    .catch((err) => {
-        console.log("connection failed", err);
-    });
+  // .connect(localDb)
+  .connect(dbUrl)
+  .then(() => {
+    console.log("database connected, connection open");
+  })
+  .catch((err) => {
+    console.log("connection failed", err);
+  });
 
 const AppError = require("./utils/AppError");
 const User = require("./models/users");
@@ -46,79 +46,79 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    touchAfter: 24*3600,
-    crypto: {
-        secret,
-    }
-})
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 3600,
+  crypto: {
+    secret,
+  },
+});
 
-store.on("error", function(e) {
-    console.log("SESSION STORE ERROR", e);
-})
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
 
 const sessionConfig = {
-    store,
-    name: "__ycs",
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
+  store,
+  name: "__ycs",
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 };
 app.use(session(sessionConfig));
 app.use(flash());
 
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
 ];
 const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
 ];
 const fontSrcUrls = [
-    "https://fonts.gstatic.com",
-    "https://fonts.googleapis.com",
+  "https://fonts.gstatic.com",
+  "https://fonts.googleapis.com",
 ];
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "'https://source.unsplash.com/",
-                "https://yelpcampbucket.s3.ap-south-1.amazonaws.com/uploads/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-            // mediaSrc: ["https://yelpcampbucket.s3.ap-south-1.amazonaws.com/uploads/"],
-        },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://source.unsplash.com/",
+        "https://yelpcampbucket.s3.ap-south-1.amazonaws.com/uploads/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+      // mediaSrc: ["https://yelpcampbucket.s3.ap-south-1.amazonaws.com/uploads/"],
+    },
+  })
 );
 
 app.use(passport.initialize());
@@ -128,53 +128,53 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.successMsg = req.flash("success");
-    res.locals.errorMsg = req.flash("error");
-    res.locals.infoMsg = req.flash("info");
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.successMsg = req.flash("success");
+  res.locals.errorMsg = req.flash("error");
+  res.locals.infoMsg = req.flash("info");
+  next();
 });
 
 app.get("/", (req, res) => {
-    res.render("index", { req });
+  res.render("index", { req });
 });
 app.use("/yelp", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.all("*", (req, res, next) => {
-    next(new AppError("Page not found", 404));
+  next(new AppError("Page not found", 404));
 });
 
 app.use((err, req, res, next) => {
-    if (err instanceof multer.MulterError) {
-        if (err.code === "LIMIT_FILE_SIZE") {
-            req.flash("error", "File size too large");
-            return res.redirect(req.originalUrl);
-        }
-
-        if (err.code === "LIMIT_FILE_COUNT") {
-            req.flash("error", "Cannot upload more than 5 images");
-            return res.redirect(req.originalUrl);
-        }
-
-        if (err.code === "LIMIT_UNEXPECTED_FILE") {
-            req.flash(
-                "error",
-                "Only jpeg, png, jpg and jfif file types are supported"
-            );
-            return res.redirect(req.originalUrl);
-        }
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      req.flash("error", "File size too large");
+      return res.redirect(req.originalUrl);
     }
 
-    const { message = "Something went wrong", status = 500 } = err;
-    res.status(status).render("errorpage", {
-        err,
-        webpageheading: `Error (${status})`,
-    });
+    if (err.code === "LIMIT_FILE_COUNT") {
+      req.flash("error", "Cannot upload more than 5 images");
+      return res.redirect(req.originalUrl);
+    }
+
+    if (err.code === "LIMIT_UNEXPECTED_FILE") {
+      req.flash(
+        "error",
+        "Only jpeg, png, jpg and jfif file types are supported"
+      );
+      return res.redirect(req.originalUrl);
+    }
+  }
+
+  const { message = "Something went wrong", status = 500 } = err;
+  res.status(status).render("errorpage", {
+    err,
+    webpageheading: `Error (${status})`,
+  });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("app listening on port", port);
+  console.log("app listening on port", port);
 });
