212df60f5307e4479a3cfe476649de4dba0f2d46
ppratham0811 <ppratham0811@gmail.com>
2023-10-16 21:48:39 +0530

    fixed aws s3 credentials error

--- aws/index.js
+++ aws/index.js
@@ -4,8 +4,10 @@ const uuid = require("uuid").v4;
 const s3 = new S3();
 
 aws.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
   region: process.env.AWS_REGION,
 });
 
